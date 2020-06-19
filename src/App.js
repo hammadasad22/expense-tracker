2d9e35401e03592a3df3233a1393f87912d02c2a
hammadasad22 <hammadasad190@gmail.com>
2020-06-19 21:26:02 +0500

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionList } from "./components/TransactionList";
+import { AddTrransaction } from "./components/AddTrransaction";
+
+import { GlobalProvider } from './context/GlobalState'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <div className="container">
+        <Header />
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTrransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
